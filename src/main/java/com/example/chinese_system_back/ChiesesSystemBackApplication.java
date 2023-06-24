90a3777dc1a4b18ccf2bd816e7ad86d3262b6fef
杨洹 <827767408@qq.com>
2023-06-24 16:26:00 +0800

    myway

---
+++ src/main/java/com/example/chinese_system_back/ChiesesSystemBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.chinese_system_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChiesesSystemBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChiesesSystemBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/chinese_system_back/ChiesesSystemBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chinese_system_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChiesesSystemBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
